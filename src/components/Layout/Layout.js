3cf1915a9db05cbc29f13fe40a8a2982de2a2ca9
David <ledavid64@gmail.com>
2018-06-13 15:19:12 -0700

    Set up project
    Add CSS Module - Folder Tree

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const layout = (props) => (
+  <React.Fragment>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+      <main>
+        {props.children}
+    </main>
+  </React.Fragment>
+)
+
+export default layout;
