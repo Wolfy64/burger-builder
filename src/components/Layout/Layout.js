da6e42b8292117f90aaa6cdac10b3855d2846f91
David <ledavid64@gmail.com>
2018-06-14 13:50:00 -0700

    Starting Implementation of the BurguerBuilder Container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from './Layout.css';
 
 const layout = (props) => (
   <React.Fragment>
     <div>Toolbar, SideDrawer, Backdrop</div>
-      <main>
+    <main className={classes.Content}>
         {props.children}
     </main>
   </React.Fragment>
