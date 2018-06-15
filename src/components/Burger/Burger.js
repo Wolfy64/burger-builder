ee785e9672edd476565ade2c7ae5aefd008edf91
David <ledavid64@gmail.com>
2018-06-15 15:22:10 -0700

    Outputting Burguer Ingredients Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import classes from './Burger.css';
-import BurguerIngredient from './BurgerIngredient/BurgerIngredient';
+import React from "react";
+import classes from "./Burger.css";
+import BurguerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = (props) => {
+const burger = props => {
+  // === Instructor Method ===
+  // const transformIngredients = Object.keys(props.ingredients)
+  //   .map(igKey => {
+  //     return [...Array(props.ingredients[igKey])].map((_, i) => {
+  //       return <BurguerIngredient key={igKey + i} type={igKey} />
+  //     })
+  //   });
+
+  // === Personnal Method ===
+  const transformIngredients = Object.entries(props.ingredients)
+    .map(ingredient => {
+      const igName = ingredient[0];
+      const igQuantity = ingredient[1];
+
+      const ingredients = (quantity, arr = []) => {
+        if (quantity === 0) return arr;
+        arr.push(<BurguerIngredient key={igName + quantity} type={igName} />);
+        return ingredients(quantity - 1, arr);
+      };
+
+      return ingredients(igQuantity);
+    }
+  );
+
   return (
-    <div className={classes.Burger} >
-      <BurguerIngredient type="bread-top"/>
-      <BurguerIngredient type="cheese"/>
-      <BurguerIngredient type="meat"/>
-      <BurguerIngredient type="bread-bottom"/>
+    <div className={classes.Burger}>
+      <BurguerIngredient type="bread-top" />
+      {transformIngredients}
+      <BurguerIngredient type="bread-bottom" />
     </div>
   );
-}
+};
 
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurguerBuilder extends Component{
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  }
   render() {
     return (
       <React.Fragment>
-        <Burger/>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </React.Fragment>
     );
