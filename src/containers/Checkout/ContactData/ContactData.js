88e685d9b667ceaea4d428fbfd63ec561fc6dc83
David <ledavid64@gmail.com>
2018-08-01 10:49:13 -0700

    Order Submission & Passing Data Between Page

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import Button from '../../../components/UI/Button/Button';
+import classes from './ContactData.css';
+import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+
+class ContactData extends Component {
+  state = {
+    name: '',
+    email: '',
+    address: {
+      street: '',
+      postalCode: ''
+    },
+    loading: false
+  };
+
+  orderHandler = event => {
+    event.preventDefault();
+    this.setState({ loading: true });
+    const order = {
+      ingredients: this.state.ingredients,
+      totalPrice: this.state.totalPrice,
+      customer: {
+        name: 'David Wolf',
+        address: {
+          street: 'TestStreet 1',
+          zipCode: '42999',
+          country: 'USA'
+        },
+        email: 'test@mail.com',
+        deliveryMode: 'fastest'
+      }
+    };
+    axios
+      .post('orders.json', order)
+      .then(response => {
+        this.setState({ loading: false });
+        this.props.history.push('/');
+      })
+      .catch(error => {
+        this.setState({ loading: false });
+      });
+    console.log(this.props.ingredients);
+  };
+
+  render() {
+    let form = (
+      <form>
+        <input
+          className={classes.Input}
+          type="text"
+          name="name"
+          placeholder="Your name"
+        />
+        <input
+          className={classes.Input}
+          type="email"
+          name="email"
+          placeholder="Your email"
+        />
+        <input
+          className={classes.Input}
+          I
+          type="text"
+          name="street"
+          placeholder="Your Street"
+        />
+        <input
+          className={classes.Input}
+          I
+          type="text"
+          name="postal"
+          placeholder="Postal Code"
+        />
+        <Button btnType="Success" clicked={this.orderHandler}>
+          ORDER
+        </Button>
+      </form>
+    );
+    if (this.state.loading) form = <Spinner />;
+    return (
+      <div className={classes.ContactData}>
+        <h4>Enter you Contact Data</h4>
+        {form}
+      </div>
+    );
+  }
+}
+
+export default ContactData;
