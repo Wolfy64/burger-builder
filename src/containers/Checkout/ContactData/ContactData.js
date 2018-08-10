d13a451d87dc14a19b58269973e0114e0d6d41a3
David <ledavid64@gmail.com>
2018-08-10 16:25:28 -0700

    Working on a Error

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -87,6 +87,7 @@ class ContactData extends Component {
           ]
         },
         value: '',
+        validation: {},
         valid: true
       }
     },
@@ -122,6 +123,10 @@ class ContactData extends Component {
 
   checkValidity(value, rules) {
     let isValid = true;
+
+    if (!rules) {
+      return true;
+    }
 
     if (rules.required) {
       isValid = value.trim() !== '' && isValid;
